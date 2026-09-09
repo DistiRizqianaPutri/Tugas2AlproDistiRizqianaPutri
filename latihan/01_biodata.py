208b6e3629eb8a9a69d969faa63be1a432cb7cb3
Disti Rizqiana Putri <distirizkianaputri@gmail.com>
2026-09-10 03:07:40 +0700

    Tugas Alpro

---
+++ latihan/01_biodata.py
@@ -0,0 +1,13 @@
+nama = "Disti Rizqiana Putri"
+nim = "2225250098"
+kelas = "3E"
+tahun_lahir = 2007
+
+TAHUN_SEKARANG = 2026
+
+umur = TAHUN_SEKARANG - tahun_lahir
+
+print(f"Nama  : {nama}")
+print(f"NIM   : {nim}")
+print(f"Kelas : {kelas}")
+print(f"Umur  : sekitar {umur} tahun")
